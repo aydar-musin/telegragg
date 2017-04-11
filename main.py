ebaee18849d394a1f3fa431cba13c932ea754a3a
Aydar Musin <21pomni@gmail.com>
2017-04-11 20:08:23 +0300

    changed user messages

--- main.py
+++ main.py
@@ -36,10 +36,24 @@ WAIT_PASSWORD = 'WAIT_PASSWORD'
 
 
 def react(state, user_id, message):
-    if message == '/start' or message == '/help':
-        return None, 'Welcome! Use /add to add email. Also you can use /list to show all your added emails and /help to see this message.'
+    if message == '/start':
+        return None, 'Hello! I can aggregate all your incoming emails and sent it to you through telegram. I use encryption and don\'t store your emails. So I\'m secure bot :)\nUse /add command to setup your email box. \nIf you have some questions or problems use /help command.'
+    if message == '/help':
+        return None, """
+        Possible commands:
+/add - add new email
+/list - show all added emails
+/help - show this message
+/start  - show bot description
+
+If you have problems with setting up your email make sure that:
+1) two factor authorization disabled on you email account
+2) enabled access by IMAP protocol
+
+Email for questions: a.musin@outlook.com
+        """
     elif message == '/add':
-        return WAIT_EMAIL, 'OK, send me your email address'
+        return WAIT_EMAIL, 'Write your email address'
     elif message == '/list':
         emails = database.get_emails(user_id)
         if emails:
@@ -50,7 +64,7 @@ def react(state, user_id, message):
     else:
         if state == WAIT_EMAIL:
             if not re.match(r'[^@]+@[^@]+\.[^@]+', message):
-                return WAIT_EMAIL, 'Not valid email address. Try again'
+                return WAIT_EMAIL, 'Not valid email. Try again'
             elif database.get_user(user_id) and message in [obj.email for obj in database.get_emails(user_id)]:
                 return WAIT_EMAIL, 'This email is already added. Try again'
             else:
@@ -58,7 +72,7 @@ def react(state, user_id, message):
                 email.email = message
                 user_temp_emails[user_id] = email
 
-                return WAIT_PASSWORD, 'OK, send me the password. It\'s secure :)'
+                return WAIT_PASSWORD, 'Write password. I use encryption to protect your data :)'
         elif state == WAIT_PASSWORD:
             temp_email = None
 
@@ -71,7 +85,7 @@ def react(state, user_id, message):
             temp_email.imap_host = 'imap.'+temp_email.email.split('@')[1]
 
             if not email_checker.check_settings(temp_email):
-                return None, 'I can not connect to your email box.'
+                return None, 'I can not connect to your email box. Check your email address and password or use /help command.'
 
             user = database.get_user(user_id)
 
