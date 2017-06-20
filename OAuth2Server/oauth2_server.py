bc0ff007edc1c579fde983598203389ec4efa767
Aydar Musin <21pomni@gmail.com>
2017-06-20 14:22:17 +0300

    first successfull google login

--- OAuth2Server/oauth2_server.py
+++ OAuth2Server/oauth2_server.py
@@ -3,7 +3,8 @@ import json
 import flask
 import httplib2
 
-from apiclient import discovery
+from oauth2client import client
+
 from oauth2client.client import OAuth2WebServerFlow
 from EmailServices import EmailServices
 import os
@@ -17,35 +18,44 @@ def oa2redirect(es_type, user_id):
     if es_type == 'gmail':
         flow = get_google_flow(user_id)
         auth_uri = flow.step1_get_authorize_url()
+        flask.session['user_id'] = user_id
+        flask.session['es_type'] = es_type
+
         return flask.redirect(auth_uri)
 
     return "Unknown es_type"
 
 @app.route('/oa2callback')
 def oa2callback():
-    if 'code' in flask.request.args and 'es_type' in flask.request.args and 'user_id' in flask.request.args:
+    if 'code' in flask.request.args and 'es_type' in flask.session and 'user_id' in flask.session:
         auth_code = flask.request.args.get('code')
-        es_type =flask.request.args.get('es_type')
-        user_id = flask.request.args.get('user_id')
+        es_type = flask.session['es_type']
+        user_id = flask.session['user_id']
+        
+        flask.session.pop('es_type', None)
+        flask.session.pop('user_id', None)
 
         if es_type == 'gmail':
             flow = get_google_flow(user_id)
             credentials = flow.step2_exchange(auth_code)
+
             return credentials.to_json()
         else:
             return "Unknown es_type"
 
 
 def get_google_flow(user_id):
-    return OAuth2WebServerFlow(client_id='1048164942148-j779t9t942gfsai79fhu8fq43cvk6i6e.apps.googleusercontent.com',
+    return client.flow_from_clientsecrets(
+        os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'EmailServices/secrets/google.json')),
+        scope='https://www.googleapis.com/auth/gmail.readonly',
+        redirect_uri=flask.url_for('oa2callback', _external=True))
+"""   return OAuth2WebServerFlow(client_id='1048164942148-j779t9t942gfsai79fhu8fq43cvk6i6e.apps.googleusercontent.com',
                            client_secret='sqpZVGgSB812NqD2a3v2OX8L',
                            scope='https://www.googleapis.com/auth/gmail.readonly',
-                           redirect_uri=flask.url_for('oa2callback', es_type='gmail', user_id=user_id ,_external=True).replace('127.0.0.1', 'lvh.me'))
-#
-#    return client.flow_from_clientsecrets(
-#                    os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'EmailServices/secrets/google.json')),
-#                    scope='https://www.googleapis.com/auth/gmail.readonly',
-#                    redirect_uri=flask.url_for('oa2callback', es_type='gmail', user_id=user_id ,_external=True))
+                           redirect_uri=flask.url_for('oa2callback', es_type='gmail', user_id=user_id ,_external=True),
+                               )
+"""
+
 
 if __name__ == '__main__':
   import uuid
